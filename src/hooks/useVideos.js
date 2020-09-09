a21e81d86e7d10cffb123a90246a3f2a8155d325
Jovan12345 <jovanj@gecko.mk>
2020-09-09 17:27:03 +0200

    ready for deploy

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import youtube from '../apis/youtube';
+
+
+const KEY = 'AIzaSyCN6e98GjAfa_d4E4mISSn8uCP6g5b1DvE';
+
+const useVideos = (defaultSearchTerm) => {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        search(defaultSearchTerm);
+    }, [defaultSearchTerm]);
+
+    const search = async term => {
+        const { data } = await youtube.get('/search', {
+            params: {
+                q: term,
+                part: 'snippet',
+                maxResults: 5,
+                type: 'video',
+                key: KEY,
+            },
+        });
+
+        setVideos(data.items)
+    }
+
+    return [videos, search];
+};
+
+export default useVideos;
